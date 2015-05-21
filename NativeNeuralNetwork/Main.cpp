6b497068fcf373ed6a52e074b834eb23ec5c9f0f
calbonaler <calbonaler.prime@gmail.com>
2015-05-21 18:11:52 +0900

    ニューロン数自動化のための変更
    コスト関数で平均をとるように変更して、ニューロン数が変更された場合に影響を受けないようにした。また、実際にニューロン数自動化のための試験的なコードを追加した。

--- NativeNeuralNetwork/Main.cpp
+++ NativeNeuralNetwork/Main.cpp
@@ -18,13 +18,18 @@ const struct
 	Floating Noise;
 } PreTrainingConfigurations[]
 {
-	{ 100, 100, 1, static_cast<Floating>(0.1) },
-	{ 100, 100, 1, static_cast<Floating>(0.2) },
-	{ 100, 100, 1, static_cast<Floating>(0.3) },
+	{ 500, 500, 1, static_cast<Floating>(0.1) },
+	{ 500, 10000, 100, static_cast<Floating>(0.2) },
+	{ 500, 10000, 100, static_cast<Floating>(0.3) },
 };
 
 const int FineTuningEpochs = 30;
 const ValueType FineTuningLearningRate = static_cast<ValueType>(0.01);
+
+// ニューロン数自動化 メモ
+// できるだけ計算をさせないことが重要
+// 最終エポックでのテストコスト予測はもちろん、ニューロン数ごとの最終テストコストも予測することで
+// チェックすべきニューロン数の組み合わせを減少させる。
 
 void TestSdA(const LearningSet& datasets)
 {
@@ -33,19 +38,39 @@ void TestSdA(const LearningSet& datasets)
 	for (int i = 0; i < 10; i++)
 	{
 		StackedDenoisingAutoEncoder sda(random(), datasets.TrainingData().Row() * datasets.TrainingData().Column());
+		ValueType lastLayerTestCost = 0;
 		for (unsigned int i = 0; i < sizeof(PreTrainingConfigurations) / sizeof(PreTrainingConfigurations[0]); i++)
 		{
+			ValueType testCost = 0;
 			for (unsigned int neurons = PreTrainingConfigurations[i].MinNeurons; neurons <= PreTrainingConfigurations[i].MaxNeurons; neurons += PreTrainingConfigurations[i].NeuronIncrement)
 			{
 				sda.HiddenLayers.Set(i, neurons);
 				std::cout << "\"" << "Number of neurons of pre-training layer " << i << " is " << neurons << "\"" << std::endl;
+				//ValueType lastEpochTestCost = 0;
 				for (unsigned int epoch = 1; epoch <= PreTrainingEpochs; epoch++)
 				{
-					ValueType costTrain = sda.HiddenLayers[i].Train(datasets.TrainingData(), PreTrainingLearningRate, PreTrainingConfigurations[i].Noise);
-					ValueType costTest = sda.HiddenLayers[i].ComputeCost(datasets.TestData(), PreTrainingConfigurations[i].Noise);
-					std::cout << epoch << " " << costTrain << " " << costTest << std::endl;
+					//ValueType last2EpochTestCost = lastEpochTestCost;
+					//lastEpochTestCost = testCost;
+					ValueType trainCost = sda.HiddenLayers[i].Train(datasets.TrainingData(), PreTrainingLearningRate, PreTrainingConfigurations[i].Noise);
+					testCost = sda.HiddenLayers[i].ComputeCost(datasets.TestData(), PreTrainingConfigurations[i].Noise);
+					// TODO: 最終エポックでのテストコストの予測が不完全であるため、早期終了は一時取りやめ
+					//if (epoch == 3 && i > 0)
+					//{
+					//	ValueType coeff = (testCost - last2EpochTestCost) / (static_cast<ValueType>(1) / epoch - static_cast<ValueType>(1) / (epoch - 2));
+					//	ValueType bias = testCost - coeff / epoch;
+					//	ValueType guessedTestCost = coeff / 15 + bias;
+					//	std::cout << epoch << " " << trainCost << " " << testCost << " " << guessedTestCost << std::endl;
+					//	if (guessedTestCost > lastLayerTestCost)
+					//		goto increaseNeuron;
+					//}
+					//else
+					std::cout << epoch << " " << trainCost << " " << testCost << " none" << std::endl;
 				}
+				if (testCost <= lastLayerTestCost)
+					break;
+			increaseNeuron:;
 			}
+			lastLayerTestCost = testCost;
 		}
 
 		sda.SetLogisticRegressionLayer(datasets.ClassCount);
